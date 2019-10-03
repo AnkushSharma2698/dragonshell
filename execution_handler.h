48429f52b231f3d4109c6e84063bb69a762e4e9e
Ankush Sharma <ankush1@ualberta.ca>
2019-10-03 17:44:22 -0600

    basically everything is working

--- execution_handler.h
+++ execution_handler.h
@@ -1,8 +1,3 @@
 #ifndef EXECUTION_HANDLER
 #define EXECUTION_HANDLER
-#include <iostream>
-#include <vector>
-void executeInstructions(std::vector<std::string> &instructions);
-void exitDragonShell();
-void  killBackgroundProcesses();
 #endif 
